eeb0659a83d6cb306df40dbe116806a9e23042e2
Carlos Berrio <4634@holbertonschool.com>
2022-10-04 10:27:26 -0500

    commit

---
+++ BE/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const products = [
+    {
+aquí va el código de la tarjeta        
+    }
+]
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
