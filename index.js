e6a2db7a15a8072b2c089b79e64b377daa2a907f
dianabanguera <dianabangueravillalobo@gmail.com>
2022-10-10 10:09:45 -0500

    boton bid

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const menu = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
+var priceUpdate = []
 
+//ej1: [25, 26, 27]
+/*ej2: [
+    {nombre: 'diana', precio: 20},
+    {nombre: 'vivi', precio: 60},
+] */
 
 menu.addEventListener('click', clickMenu)
 
-function clickMenu () {
+function clickMenu() {
     menuMobile.classList.toggle('inactive')
 }
+
+/*function priceDuplicate(list){
+    const result = list.reduce((acc,item)=>{
+        if(!acc.includes(item)){
+            acc.push(item);
+        }
+        return acc;
+      },[])
+}*/
+function agregarElementos() {
+    var lista = document.getElementById("ulListado");
+   // priceDuplicate(priceUpdate)
+    priceUpdate.forEach(function (data, index) {
+        var linew = document.createElement("li");
+        var contenido = document.createTextNode(data);
+        lista.appendChild(linew);
+        linew.appendChild(contenido);
+        info.reset();
+    })
+}
+
+function updatePrice(price) {
+    if (price.value) {
+        priceUpdate.unshift(+price.value)
+        console.log('arr', priceUpdate)
+        agregarElementos();
+    }
+    info.reset();
+}
+
