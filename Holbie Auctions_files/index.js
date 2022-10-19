d85fc14b387337e09fc9fe557ee4df0e7c4a0ce7
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-10-19 10:30:20 -0500

    Delete index.js

--- Holbie Auctions_files/index.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.menu');
-const menuMobile = document.querySelector('.mobile-menu');
-
-
-menu.addEventListener('click', clickMenu)
-
-function clickMenu () {
-    menuMobile.classList.toggle('inactive')
-}
