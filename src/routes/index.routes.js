7543722c54f8bfc6415091f73c4153c025f2539a
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-10-19 10:36:31 -0500

    Delete src/routes directory

--- src/routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/index.controllers.js')
-
-router.get('/home', controller.index)
-
-module.exports = router
