419994aceed2259cb791eb2d87ea1fc15a3b69e1
Carlos Berrio <4634@holbertonschool.com>
2022-10-19 16:07:51 -0500

    update

--- src/index.js
+++ src/index.js
@@ -11,12 +11,15 @@ function clickMenu() {
 
 function getProducts() {
     const Http = new XMLHttpRequest();
-    const url='http://localhost:4000/all';
+    const url='http://localhost:4000/api/all';
     Http.open("GET", url);
+    // Http.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:4000/api/all");
     Http.send();
-
     Http.onreadystatechange = (e) => {
     console.log(Http.responseText)
+    // document.querySelector('body').innerHTML = Http.responseText;
+    // document.querySelector('body').innerHTML = `
+    //     <h1>${Http.response[0]['DEPENDENCIA']}</h1>`
 }
 }
 
