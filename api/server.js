6f85e4865290b383804f1d22dff147d287faeb4e
Carlos Berrio <4634@holbertonschool.com>
2022-10-19 16:06:45 -0500

    update

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-Parser')
-const Products = require ("./api/apiproducts");
+const Products = require ("./router/apiproducts");
+const cors = require('cors');
 
 const app = express();
 const mongoose = require('mongoose');
-
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
